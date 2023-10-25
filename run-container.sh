063e8e659757ffdea6ed54ca18f6d3e512209fde
And Paiva <andre.pcr@hotmail.com>
2023-10-25 08:06:30 +0000

    Update run-container script

--- run-container.sh
+++ run-container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run -p 8888:8888 -p 6006:6006 -it --rm \
-    -v ./va/:/app/VocalAssignment-SSCS \
-    -v ./mpe/:/app/MultiF0 \
+    -v ./va_holdout/:/app/va_holdout \
+    -v ./va_kfold/:/app/va_kfold \
+    -v ./mpe/:/app/mpe \
     --gpus all \
     tf:v1
