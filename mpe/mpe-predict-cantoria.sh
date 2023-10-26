7ac8776f08bc3c4dacdf98764ed1892eeb6c0939
And Paiva <andre.pcr@hotmail.com>
2023-10-26 06:41:56 +0000

    Updated mpe prediction script

--- mpe/mpe-predict-cantoria.sh
+++ mpe/mpe-predict-cantoria.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python predict_on_audio.py --audio_folder '../va/Datasets/Cantoria/Audio'
+python predict_on_audio.py --audio_folder '../va_holdout/Datasets/Cantoria/Audio'
+python predict_on_audio.py --audio_folder '../va_kfold/Datasets/Cantoria/Audio'
