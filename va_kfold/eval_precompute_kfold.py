220e966449f7f3cfd28cd14ee30d18e4511489a9
And Paiva <andre.pcr@hotmail.com>
2023-11-05 18:28:18 +0000

    eval_precompute_kfold fixed

--- va_kfold/eval_precompute_kfold.py
+++ va_kfold/eval_precompute_kfold.py
@@ -10,7 +10,7 @@ for i in range(1, 6):
     log_folder = 'mask_voas_v2'
     ckpt_dir = './Checkpoints/mask_voas_v2_treino2_kfold_' + str(i) + '.keras'        
     va_utils.load_weights(model, ckpt_dir=ckpt_dir)
-    metrics_dir = './Evaluation_Data/' + log_folder + '_treino1_kfold_' + str(i) + '.h5'
+    metrics_dir = './Evaluation_Data/' + log_folder + '_treino2_kfold_' + str(i) + '.h5'
     #songlist = va_utils.pick_songlist(first = 800*(i-1), amount=800, split='train')
     #metrics = va_utils.metrics_val_precompute(model, metrics_dir, songlist)
     metrics = va_utils.metrics_test_precompute(model, metrics_dir)
