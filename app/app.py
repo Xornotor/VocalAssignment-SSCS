b64ae05020de1d7523f240f1b3729a0bfaab3293
André Paiva <andre.pcr@hotmail.com>
2026-04-12 04:22:58 +0000

    Webapp share=True

--- app/app.py
+++ app/app.py
@@ -8,4 +8,4 @@ cqfe_interface = gr.Interface(fn=cqfe,
                               description="An application that uses Multi-Pitch Estimation and Voice Assignment to transform audio files with Choral Quartets recordings into files (CSV, HDF5 and MIDI) containing F0 estimations for each voice (Soprano, Alto, Tenor and Bass). The processing may take a few minutes.")
 
 if __name__ == "__main__":
-    cqfe_interface.launch()
+    cqfe_interface.launch(share=True)
